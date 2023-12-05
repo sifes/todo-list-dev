c8edee32049aaa1092328457766ca4796f7ee6e5
sifes <lonfark@gmail.com>
2023-12-05 20:01:28 +0200

    styles: add theme

--- next-todo/src/app/layout.tsx
+++ next-todo/src/app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.scss';
-
-const inter = Inter({ subsets: ['latin'] })
+import { Providers } from './providers';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Todo List',
   description: 'Todo List',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
--- next-todo/src/components/theme-toggle/ThemeToggle.tsx
+++ next-todo/src/components/theme-toggle/ThemeToggle.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import dayIcon from '../../assets/day.svg';
 import nightIcon from '../../assets/night.svg';
+import { useTheme } from 'next-themes';
 interface ThemeToggleProps {}
 
 export const ThemeToggle: React.FC<ThemeToggleProps> = ({}) => {
-  console.log(nightIcon);
+  const [mounted, setMounted] = React.useState(false);
+  const { theme, setTheme } = useTheme();
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   return (
-    <button className='toggle-theme' onClick={() => {}}>
-      <img src={nightIcon.src} alt='toggle theme' />
+    <button
+      className='toggle-theme'
+      onClick={() => (theme === 'light' ? setTheme('dark') : setTheme('light'))}
+    >
+      <img src={theme === 'light' ? nightIcon.src : dayIcon.src} alt='toggle theme' />
     </button>
   );
 };
